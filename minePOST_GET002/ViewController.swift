5e6c1c1e2210cb66582748dda85fff14461afa87
FujimuraShin <sfujimura2@gmail.com>
2021-05-12 13:14:00 +0900

    Initial Commit

---
+++ minePOST_GET002/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  minePOST_GET002
+//
+//  Created by mf-osaka on 2021/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
